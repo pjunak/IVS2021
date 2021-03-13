e4b3854846c069b9c103cff20db11cf310cfaefe
Patrik Haas <xhaasp00@vutbr.cz>
2021-03-13 16:09:58 +0100

    Pridanie atributov a metod v MainWindowClass

--- src/Triedy/MainWindowClass.cs
+++ src/Triedy/MainWindowClass.cs
@@ -1,8 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 
-public class MainWindowClass
+public class MainWindowClass : INotifyPropertyChanged
 {
+	private string _vstup;
+	public string Vstup
+	{
+		get { return _vstup; }
+		set
+		{
+			_vstup = value;
+		}
+	}
+
+	public List<string> Vstupy { get; set;}
+	public List<string> Vysledky {get; set;}
+
 	public MainWindowClass()
 	{
+		for (int i = 0; i < 4; i++)
+		{
+			Vysledky.Add("");
+			Vstupy.Add("");
+		}
+	}	
+
+	public event PropertyChangedEventHandler PropertyChanged;
+	public void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+	{
+		var handler = PropertyChanged;
+		if (handler != null)
+		{
+			handler(this, new PropertyChangedEventArgs(propertyName));
+		}
 	}
 }
