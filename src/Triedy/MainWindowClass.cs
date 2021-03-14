22c74aa1828c04ced0514d6102a808a637001da7
pjunak <junakpetr@gmail.com>
2021-03-14 16:43:04 +0100

    PostFix work in progress

--- src/Triedy/MainWindowClass.cs
+++ src/Triedy/MainWindowClass.cs
@@ -14,16 +14,21 @@ public class MainWindowClass : INotifyPropertyChanged
 	}
 
 	public List<string> Vstupy { get; set;}
+	public List<string> VstupyPostFix { get; set; }
 	public List<string> Vysledky {get; set;}
 
 	public MainWindowClass()
 	{
 		for (int i = 0; i < 4; i++)
 		{
-			Vysledky.Add("");
 			Vstupy.Add("");
+			VstupyPostFix.Add("");
+			Vysledky.Add("")
 		}
-	}	
+	}
+
+	Vstupy[0] = "2*5-10/5";
+	Debug.WriteLine("{0}", Vstupy[0]);
 
 	public event PropertyChangedEventHandler PropertyChanged;
 	public void RaisePropertyChanged([CallerMemberName] string propertyName = null)
--- src/Triedy/ToPostfixClass.cs
+++ src/Triedy/ToPostfixClass.cs
@@ -1,8 +1,106 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public class ToPostfixClass
 {
-	public ToPostfixClass()
-	{
-	}
+    internal static int Prec(char ch)
+    {
+        switch (ch)
+        {
+            case '+':
+            case '-':
+                return 1;
+
+            case '*':
+            case '/':
+                return 2;
+
+            case '^':
+                return 3;
+        }
+        return -1;
+    }
+
+    public ToPostfixClass()
+    {
+        List<char> primeNumbers = new List<int>();
+        for (char ch: MainWindowClass.Vstupy[0])
+        {
+
+        }
+
+        public static string infixToPostfix(string exp)
+        {
+            // initializing empty String for result  
+            string result = "";
+
+            // initializing empty stack  
+            Stack<char> stack = new Stack<char>();
+
+            for (int i = 0; i < exp.Length; ++i)
+            {
+                char c = exp[i];
+
+                // If the scanned character is an  
+                // operand, add it to output.  
+                if (char.IsletterOrDigit(c))
+                {
+                    result += c;
+                }
+
+                // If the scanned character is an '(', 
+                // push it to the stack.  
+                else if (c == '(')
+                {
+                    stack.Push(c);
+                }
+
+                //  If the scanned character is an ')',  
+                // pop and output from the stack   
+                // until an '(' is encountered.  
+                else if (c == ')')
+                {
+                    while (stack.Count > 0 &&
+                            stack.Peek() != '(')
+                    {
+                        result += stack.Pop();
+                    }
+
+                    if (stack.Count > 0 && stack.Peek() != '(')
+                    {
+                        return "Invalid Expression"; // invalid expression 
+                    }
+                    else
+                    {
+                        stack.Pop();
+                    }
+                }
+                else // an operator is encountered 
+                {
+                    while (stack.Count > 0 && Prec(c) <=
+                                      Prec(stack.Peek()))
+                    {
+                        result += stack.Pop();
+                    }
+                    stack.Push(c);
+                }
+
+            }
+
+            // pop all the operators from the stack  
+            while (stack.Count > 0)
+            {
+                result += stack.Pop();
+            }
+
+            return result;
+        }
+
+        // Driver method   
+        public static void Main(string[] args)
+        {
+            string exp = "a+b*(c^d-e)^(f+g*h)-i";
+            Console.WriteLine(infixToPostfix(exp));
+        }
+    }
 }
