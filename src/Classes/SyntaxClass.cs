a235005fae083587f8a252871c3156ad62b54428
Dalibor Čásek <dalibranar@gmail.com>
2021-04-07 22:41:51 +0200

    Dokončení implementace SyntaxClass.

--- src/Classes/SyntaxClass.cs
+++ src/Classes/SyntaxClass.cs
@@ -4,19 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 namespace Calculator.Classes
 {
     class SyntaxClass
     {
+        public SyntaxClass() { }
+
         private enum Row
         {
             DotComma = 0,
             Pi = 1,
             PlusMinusMulDivPow = 2,
-            OBracketSin = 3,
-            Digit = 4,
-            CBracketFact = 5,
+            CBracket = 3,
+            Sin = 4,
+            Digit = 5,
+            CBracketFact = 6,
         }
 
         private enum Column
@@ -36,12 +40,13 @@ namespace Calculator.Classes
 
         //true == Zakázaný následující znak.
         //false == Povolený následující znak.
-        readonly bool[,] IncorrectFollow = new bool[6, 10]
+        readonly bool[,] IncorrectFollow = new bool[7, 10]
         {
             {false, true, true, true, true, true, true, true, true, true},
             {true, true, true, false, false, false, false, true, true, false},
             {false, false, false, true, true, true, true, false, false, true},
             {false, false, false, false, true, true, true, false, false, true},
+            {true, true, true, true, true, true, true, true, false, true},
             {false, false, true, false, false, false, false, true, true, false},
             {true, true, true, false, false, false, false, true, true, false}
         };
@@ -55,34 +60,67 @@ namespace Calculator.Classes
          * @param FinalChecking Určuje, zda se jedná o průběžnou (\c FALSE) nebo finální (\c TRUE) konrolu před výpočtem výsledku.
          *
          */
-        private void SyntaxCheck(string Input, bool FinalChecking)
+        public string SyntaxCheck(string Input, bool FinalChecking)
         {
             int InputLen = Input.Length;
             Row RSymbol;
             Column CSymbol;
 
+            //Kontrola, jestli výraz končí povolenými znaky.
             if (FinalChecking)
             {
                 IdentifyChar(Input[InputLen - 1], Input[InputLen - 1], out RSymbol, out CSymbol);
                 //CSymbol je nepoužívaná hodnota.
                 if (CannotEnd[(int)RSymbol])
                 {
-                    /**
-                     * @todo Ošetřit chybu při nesprávném ukončujícím znaku.
-                     */
+                    MessageBox.Show("Error: Výraz končí nepovolenými znaky.\nKonec.");
+                    Application.Current.Shutdown();
+                    return null;
                 }
             }
 
+            //Kontorla počtu závorek.
+            int OpenBracketCount = 0;
+            int ClosedBracketCount = 0;
+            for (int i = 0; i < InputLen; i++)
+            {
+                if (Input[i] == '(')
+                {
+                    OpenBracketCount++;
+                }
+                if (Input[i] == ')')
+                {
+                    ClosedBracketCount++;
+                }
+            }
+            if (ClosedBracketCount > OpenBracketCount)
+            {
+                MessageBox.Show("Error: Příliš mnoho uzavíracích závorek!\nKonec.");
+                Application.Current.Shutdown();
+                return null;
+            }
+            else if (OpenBracketCount > ClosedBracketCount && FinalChecking)
+            {
+                for(int i = OpenBracketCount - ClosedBracketCount; i > 0; i--)
+                {
+                    Input += ")";
+                }
+            }
+
+            //Kontrola korektní posloupnosti zanků a závorkou za sinusem.
             for (int i = 0; i < InputLen - 1; i++)
             {
                 IdentifyChar(Input[i], Input[i + 1], out RSymbol, out CSymbol);
                 if (IncorrectFollow[(int)RSymbol, (int)CSymbol])
                 {
-                    /**
-                     * @todo Ošetřit chybu při nesprávném následujícím znaku.
-                     */
+                    MessageBox.Show("Error: Špatná posloupnost znaků!\nKonec.");
+                    Application.Current.Shutdown();
+                    return null;
                 }
             }
+            
+            //V pořádku.
+            return Input;
         }
 
         /** 
@@ -116,8 +154,10 @@ namespace Calculator.Classes
                     RSymbol = Row.CBracketFact;
                     break;
                 case 's':
+                    RSymbol = Row.Sin;
+                    break;
                 case '(':
-                    RSymbol = Row.OBracketSin;
+                    RSymbol = Row.CBracket;
                     break;
                 case '+':
                 case '-':
